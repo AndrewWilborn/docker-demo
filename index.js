4d32ade91150c7b4b25afe84390ba66c238ce92f
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-31 09:59:36 -0400

    Set up simple api and docker container

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Our API is working!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
